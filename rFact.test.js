97427ae0b8ee290265ba2650bf5c145e875f2d6c
Your Name <you@example.com>
2020-01-29 13:17:48 +0700

    fix bug in test to fract

--- rFact.test.js
+++ rFact.test.js
@@ -4,10 +4,15 @@ test('rFact from 0 to equal 1', () => {
   expect(rFact(0)).toBe(1);
 });
 
-test('rFact from 3 to equal 5', () => {
-  expect(rFact(3)).toBe(5);
+test('rFact from 3 to equal 6', () => {
+  expect(rFact(3)).toBe(6);
 });
 
 test('rFact from 10 to equal 3628800', () => {
   expect(rFact(10)).toBe(3628800);
 });
+
+test('rFact from 1 to equal 1', () => {
+  expect(rFact(1)).toBe(1);
+});
+
